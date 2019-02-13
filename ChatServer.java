0bd185a9fc5b31eafcf734c7c4b1a2613bbe3ff3
fatemaMO <45967448+fatemaMO@users.noreply.github.com>
2019-02-13 13:50:25 +0200

    The following code sample is for creating a simple one-to-one client/server application,:
     where each machine sends out a string and receives a string
    for server i do
    Create a server socket
    (bind the service to a certain port)
    2. Listen for connections
    3. Accept connection and transfer the
    client request to a virtual port.
    4. Obtain input and output streams
    5. Send and receive data
    6. Terminate connection
    (after communication has ended)

---
+++ ChatServer.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Serverpk;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author HD
+ */
+public class ChatServer {
+    ServerSocket server; 
+     
+    public static void main(String[] args){
+    new ChatServer();
+                                          }
+    
+    public ChatServer(){
+    
+        try
+         {
+    server =new ServerSocket(5005);
+    while (true)
+    {
+    Socket s=server.accept();
+    new ChatHandler(s);
+    }
+         } catch (IOException ex) {
+            Logger.getLogger(ChatServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    
+    }    
+}
+
+class ChatHandler extends Thread
+{
+       DataInputStream dis;
+         PrintStream ps;
+         
+static Vector<ChatHandler> clientsVector=new Vector<ChatHandler>();
+
+public ChatHandler( Socket sc){
+    try{
+    dis= new DataInputStream(sc.getInputStream());
+    ps=new PrintStream(sc.getOutputStream());
+    clientsVector.add(this);
+    start();
+    }      catch (IOException ex) {
+               Logger.getLogger(ChatHandler.class.getName()).log(Level.SEVERE, null, ex);
+           }
+      } 
+    public void run()
+{
+     while(true)
+      {
+       String str;
+         try {
+             str = dis.readLine();
+               sendMessageToAll(str);
+         } catch (IOException ex) {
+             Logger.getLogger(ChatHandler.class.getName()).log(Level.SEVERE, null, ex);
+         }
+        
+ 
+      }
+}
+        void sendMessageToAll(String msg)
+{
+            for(ChatHandler ch: clientsVector)
+             {
+              ch.ps.println(msg);
+                }
+             }
+}
+
+
+
